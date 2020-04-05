5dc46b02897cf8eb5f8405c46524a0aaa9c65f29
jean-baptiste Goujon <jb@localhost.localdomain>
2020-04-05 15:17:42 +0300

    Ajout overload fonctions egalite, difference et outstream.

--- src/vec.hpp
+++ src/vec.hpp
@@ -1,4 +1,7 @@
+#include <cmath>
+
 namespace rtc{
+    constexpr auto EPSILON = 1e-9;
     struct vec{
         float m_x;
         float m_y;
@@ -9,6 +12,20 @@ namespace rtc{
         vec(float x, float y, float z, float w): m_x{x}, m_y{y}, m_z{z}, m_w{w} {};
 
         [[nodiscard]] inline bool is_point() const { return this->m_w > 0.0;};
+
+        bool operator==(const vec& other) const {
+           return std::abs(this->m_x - other.m_x) < EPSILON && std::abs(this->m_y - other.m_y) < EPSILON && std::abs(this->m_z - other.m_z) < EPSILON && std::abs(this->m_w - other.m_w) < EPSILON;};
+
+        bool operator!=(const vec& other) const {
+            return !(*this == other);
+        };
+
+        friend std::ostream& operator<<(std::ostream& stream, const vec& vec);
+    };
+
+    std::ostream& operator<<(std::ostream& os, const vec& vec){
+        os << vec.m_x << ' ' << vec.m_y << ' ' << vec.m_z << ' ' << vec.m_w;
+        return os;
     };
 
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,26 +1,28 @@
 #define BOOST_TEST_MODULE test boost framework
 #include <boost/test/included/unit_test.hpp>
+
+#include <cmath>
 #include "vec.hpp"
 
 using namespace boost::test_tools;
 
-auto EPSILON = tolerance(1e-6);
+auto EPSILON = tolerance(1e-12);
 
 BOOST_AUTO_TEST_SUITE(creating_a_vec_struct)
 
 BOOST_AUTO_TEST_CASE(can_be_done_specifying_all_coordinates){
-    rtc::vec point{1.0, 1.0, 4.4, 1.0};
+    rtc::vec point{1.0f, 1.0f, 4.4f, 1.0f};
 
-    BOOST_TEST_REQUIRE(point.m_x == 1.0, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_y == 1.0, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_z == 4.4, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_w == 1.0, EPSILON);
+    BOOST_TEST_REQUIRE(point.m_x == 1.0f, EPSILON);
+    BOOST_TEST_REQUIRE(point.m_y == 1.0f, EPSILON);
+    BOOST_TEST_REQUIRE(point.m_z == 4.4f, EPSILON);
+    BOOST_TEST_REQUIRE(point.m_w == 1.0f, EPSILON);
 }
 
 BOOST_AUTO_TEST_CASE(is_created_as_point_by_defautl){
-    rtc::vec defaut{2.43, 3.12, 4.21};
+    rtc::vec defaut{2.43f, 3.12f, 4.21f};
 
-    BOOST_TEST_REQUIRE(defaut.m_w == 1.0, EPSILON);
+    BOOST_TEST_REQUIRE(defaut.m_w == 1.0f, EPSILON);
     BOOST_TEST_REQUIRE(defaut.is_point() == true);
 }
 
@@ -45,6 +47,21 @@ BOOST_AUTO_TEST_CASE(to_create_a_vector){
     auto vector = rtc::vector(2.0, 4.0, 4.5);
 
     BOOST_TEST_REQUIRE(vector.is_point() == false);
-    BOOST_TEST_REQUIRE(vector.m_w == 0.0);
+    BOOST_TEST_REQUIRE(vector.m_w == 0.0f);
 }
+
+BOOST_AUTO_TEST_CASE(compairing_two_vec){
+    rtc::vec reference{2.4324, 321.413, 213.5466, 0.0};
+
+    rtc::vec other_vec{2.4324, 321.413, 213.5466, 0.0};
+    BOOST_TEST_REQUIRE(reference == other_vec);
+    BOOST_TEST_REQUIRE(reference == reference);
+
+    rtc::vec different{1.2, 1.2, 1.2};
+    BOOST_TEST_REQUIRE(reference != different);
+
+    rtc::vec infinity{INFINITY, INFINITY, INFINITY};
+    BOOST_TEST_REQUIRE(reference != infinity);
+}
+
 BOOST_AUTO_TEST_SUITE_END()

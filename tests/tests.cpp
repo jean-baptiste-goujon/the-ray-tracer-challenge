4c2ab93b07e3bbd284e1dec00beb32047e4c3e28
jean-baptiste Goujon <jb@fedora.home>
2020-12-18 19:48:07 +0200

    Remplacement de Boost Test par Catch2.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,67 +1,54 @@
-#define BOOST_TEST_MODULE test boost framework
-#include <boost/test/included/unit_test.hpp>
+#include <catch2/catch_all.hpp>
 
 #include "rtc/vec.hpp"
 
-using namespace boost::test_tools;
+using namespace Catch::literals;
 
-auto EPSILON = tolerance(1e-12);
-
-BOOST_AUTO_TEST_SUITE(creating_a_vec_struct)
-
-BOOST_AUTO_TEST_CASE(can_be_done_specifying_all_coordinates){
+TEST_CASE("Can be done specifying all coordinates", "[creating a vec struct]") {
     rtc::vec point{1.0f, 1.0f, 4.4f, 1.0f};
 
-    BOOST_TEST_REQUIRE(point.m_x == 1.0f, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_y == 1.0f, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_z == 4.4f, EPSILON);
-    BOOST_TEST_REQUIRE(point.m_w == 1.0f, EPSILON);
+    REQUIRE(point.m_x == 1.0_a);
+    REQUIRE(point.m_y == 1.0_a);
+    REQUIRE(point.m_z == 4.4_a);
+    REQUIRE(point.m_w == 1.0_a);
 }
 
-BOOST_AUTO_TEST_CASE(is_created_as_point_by_defautl){
+TEST_CASE("is created as point by default", "[creating a vec struct]") {
     rtc::vec defaut{2.43f, 3.12f, 4.21f};
 
-    BOOST_TEST_REQUIRE(defaut.m_w == 1.0f, EPSILON);
-    BOOST_TEST_REQUIRE(defaut.is_point() == true);
+    REQUIRE(defaut.m_w == 1.0_a);
+    REQUIRE(defaut.is_point() == true);
 }
 
-BOOST_AUTO_TEST_CASE(when_w_coordinate_is_0_then_considered_as_vector){
+TEST_CASE("when w coordinate is 0 then considered as vector", "[creating a vec struct]") {
     rtc::vec vector{1.0, 1.0, 1.0, 0.0};
 
-    BOOST_TEST_REQUIRE(vector.is_point() == false);
+    REQUIRE(vector.is_point() == false);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-
-BOOST_AUTO_TEST_SUITE(helper_functions)
-
-BOOST_AUTO_TEST_CASE(to_create_a_point){
+TEST_CASE("to create a point", "[helper functions]") {
     auto point = rtc::point(2.0, 4.0, 4.5);
 
-    BOOST_TEST_REQUIRE(point.is_point() == true);
+    REQUIRE(point.is_point() == true);
 }
 
-BOOST_AUTO_TEST_CASE(to_create_a_vector){
+TEST_CASE("to create a vector", "[helper functions]") {
     auto vector = rtc::vector(2.0, 4.0, 4.5);
 
-    BOOST_TEST_REQUIRE(vector.is_point() == false);
-    BOOST_TEST_REQUIRE(vector.m_w == 0.0f);
+    REQUIRE(vector.is_point() == false);
+    REQUIRE(vector.m_w == 0.0f);
 }
 
-BOOST_AUTO_TEST_CASE(compairing_two_vec){
+TEST_CASE("compairing two vec", "[helper functions]") {
     rtc::vec reference{2.4324, 321.413, 213.5466, 0.0};
 
     rtc::vec other_vec{2.4324, 321.413, 213.5466, 0.0};
-    BOOST_TEST_REQUIRE(reference == other_vec);
-    BOOST_TEST_REQUIRE(reference == reference);
+    REQUIRE(reference == other_vec);
+    REQUIRE(reference == reference);
 
     rtc::vec different{1.2, 1.2, 1.2};
-    BOOST_TEST_REQUIRE(reference != different);
+    REQUIRE(reference != different);
 
     rtc::vec infinity{INFINITY, INFINITY, INFINITY};
-    BOOST_TEST_REQUIRE(reference != infinity);
+    REQUIRE(reference != infinity);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
-
--- tests/tests_vec_operations.cpp
+++ tests/tests_vec_operations.cpp
@@ -1,68 +1,63 @@
-#include <boost/test/unit_test.hpp>
+#include <catch2/catch_all.hpp>
+
 #include "rtc/vec.hpp"
 
-
-BOOST_AUTO_TEST_SUITE(vec_operations)
-BOOST_AUTO_TEST_CASE(adding_vector_to_point_gives_new_point){
+TEST_CASE("Adding vector to point gives new point", "[vec operations]") {
     rtc::vec start{3.0, 4.0, 5.0};
     rtc::vec vector{1.0, 2.0, 3.0, 0.0};
 
     rtc::vec expected_result{4.0, 6.0, 8.0, 1.0};
-    BOOST_TEST_REQUIRE(start + vector == expected_result);
+    REQUIRE(start + vector == expected_result);
 }
 
-BOOST_AUTO_TEST_CASE(adding_two_vectors_gives_a_new_vector){
+TEST_CASE("Adding two vectors gives new vector", "[vec operations]") {
     rtc::vec first_vec{1.0, 2.0, 3.0, 0.0};
     rtc::vec second_vec{1.0, 2.0, 3.0, 0.0};
 
-    BOOST_TEST_REQUIRE(first_vec+second_vec == rtc::vector(2.0, 4.0, 6.0));
+    REQUIRE(first_vec+second_vec == rtc::vector(2.0, 4.0, 6.0));
 }
 
-BOOST_AUTO_TEST_CASE(substracting_two_points_gives_a_vector){
+TEST_CASE("Substracting two points gives a vector", "[vec operations]") {
     rtc::vec first_point{1.0, 2.0, 3.0};
     rtc::vec second_point{1.0, 3.0, 4.0};
 
-    BOOST_TEST_REQUIRE(second_point - first_point == rtc::vector(0.0, 1.0, 1.0));
+    REQUIRE(second_point - first_point == rtc::vector(0.0, 1.0, 1.0));
 }
 
-BOOST_AUTO_TEST_CASE(substracting_a_vector_to_a_point_gives_a_point){
-   rtc::vec point{1.0, 2.0, 3.0}; 
-   rtc::vec movement=rtc::vector(2.0, 4.0, 6.0);
+TEST_CASE("Substracting a vector to a point gives a point", "[vec operations]") {
+    rtc::vec first_point{1.0, 2.0, 3.0};
+    rtc::vec second_point{1.0, 3.0, 4.0};
 
-   BOOST_TEST_REQUIRE(point - movement == rtc::point(-1.0, -2.0, -3.0));
+    REQUIRE(second_point - first_point == rtc::vector(0.0, 1.0, 1.0));
 }
 
-BOOST_AUTO_TEST_CASE(substracting_two_vectors_gives_a_vector){
+TEST_CASE("Substracting two vectors gives a vector", "[vec operations]") {
     rtc::vec first_vector = rtc::vector(2.0, 4.0, 5.0);
     rtc::vec second_vector = rtc::vector(1.0, 3.0, 4.0);
 
-    BOOST_TEST_REQUIRE(first_vector - second_vector == rtc::vector(1.0, 1.0, 1.0));
+    REQUIRE(first_vector - second_vector == rtc::vector(1.0, 1.0, 1.0));
 }
 
-BOOST_AUTO_TEST_CASE(negating_a_vec_gives_its_opposite)
-{
+TEST_CASE("Negating a vec gives its opposite", "[vec operations]") {
     rtc::vec point{2.0, 1.0, 0.2};
 
     auto expected_negated = rtc::vec{-2.0, -1.0, -0.2, -1.0};
-    BOOST_TEST_REQUIRE(-point == expected_negated);
+    REQUIRE(-point == expected_negated);
 }
 
-BOOST_AUTO_TEST_CASE(vec_supports_scalar_product)
-{
+TEST_CASE("vec supports scalar products", "[vec operations]") {
     auto point = rtc::point(1.2, 2.3, 2.3);
     auto expected_result = rtc::vec{2.4, 4.6, 4.6, 2.0};
-    BOOST_TEST_REQUIRE(point * 2 == expected_result);
+    REQUIRE(point * 2 == expected_result);
 
     expected_result = rtc::vec{0.6, 1.15, 1.15, 0.5};
-    BOOST_TEST_REQUIRE(point * 0.5 == expected_result);
+    REQUIRE(point * 0.5 == expected_result);
 }
 
-BOOST_AUTO_TEST_CASE(vec_supports_scalar_division)
-{
+TEST_CASE("vec supports scalar division", "[vec operations]") {
     auto point = rtc::point(1.2, 2.3, 2.3);
     auto expected_result = rtc::vec{0.6, 1.15, 1.15, 0.5};
     
-    BOOST_TEST_REQUIRE(point / 2 == expected_result);
+    REQUIRE(point / 2 == expected_result);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
